62399b2d032744bf31d1dd63d71f4daf1e2e43c0
RosieBaroni <rosi.baroni@gmail.com>
2021-11-17 08:33:35 -0300

    exercices 5.1 - finalizados

--- fundamentals/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercises/script.js
+++ fundamentals/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercises/script.js
@@ -18,31 +18,31 @@ classMainContent();
 
 // 3.
 function classCenterContent() {
-let centerContentBackground = document.getElementsByClassName("center-content")[0];
-centerContentBackground.style.backgroundColor = "white";
+  let centerContentBackground = document.getElementsByClassName("center-content")[0];
+  centerContentBackground.style.backgroundColor = "white";
 };
 
 classCenterContent();
 
 // 4. 
 function tagH1() {
-let title = document.getElementsByTagName("h1")[0];
-title.innerText = "Exercício 5.1 - JavaScript";
+  let title = document.getElementsByTagName("h1")[0];
+  title.innerText = "Exercício 5.1 - JavaScript";
 };
 
 tagH1();
 
 // 5. 
 function tagP1() {
-let centerContentP1 = document.getElementsByTagName("p")[0];
-centerContentP1.innerText = centerContentP1.innerText.toUpperCase();
+  let centerContentP1 = document.getElementsByTagName("p")[0];
+  centerContentP1.innerText = centerContentP1.innerText.toUpperCase();
 };
 
 tagP1();
 
 // 6.
 function allParagraphs() {
-let allP = document.getElementsByTagName('p');
+  let allP = document.getElementsByTagName('p');
   for (let i = 0; index < allP.length; i += 1) {
     console.log(allP[i].innerText);
   };
