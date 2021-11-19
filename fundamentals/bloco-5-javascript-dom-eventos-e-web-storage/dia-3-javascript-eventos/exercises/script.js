d113f927eb021bc358d875f81221b063eeca8d7e
RosieBaroni <rosi.baroni@gmail.com>
2021-11-19 16:01:18 -0300

    exercises 4.3 wip

--- fundamentals/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/script.js
+++ fundamentals/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/exercises/script.js
@@ -16,7 +16,7 @@ createDaysOfTheWeek();
 // Escreva seu código abaixo.
 
 //1.
-function createDaysOfTheMonth() {
+function createDaysOfTheMonth(dayList) {
 
   const daysDec = document.querySelector('#days');
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
@@ -24,6 +24,7 @@ function createDaysOfTheMonth() {
   for (let index = 0; index < dezDaysList.length; index += 1) {
     const daysMonth = dezDaysList[index];
     const dayList = document.createElement('li');
+    
     dayList.innerHTML = daysMonth;
     dayList.className = 'day';
 
@@ -41,10 +42,57 @@ function createDaysOfTheMonth() {
 createDaysOfTheMonth();
 
 //2.
-function createHolidayButton(buttonName) {
-  const getButtonsContainer = document.querySelector('.buttons-container');
-  const holidayButton = document.createElement('button');
-  holidayButton.innerHTML = buttonName;
-  holidayButton.id = 'btn-holiday';
-  getButtonsContainer.appendChild(holidayButton);
+function createButton(text, id) {
+  const buttonContainer = document.querySelector('.buttons-container');
+  const button = document.createElement('button');
+  
+  button.innerHTML = text;
+  button.id = id;
+  buttonContainer.appendChild(button);
 };
+
+createButton('Feriados', 'btn-holiday');
+
+//3.
+/** Souce: https://developer.mozilla.org/pt-BR/docs/Web/API/EventTarget/addEventListener */
+function selectDaysHoliday() {
+  const buttonSelect = document.getElementById('btn-holiday');
+
+  buttonSelect.addEventListener('click', function() {
+    const daysHoliday = document.querySelectorAll('.holiday');
+    let showHoliday = false;
+
+    daysHoliday.forEach(function(item) {
+      item.style.backgroundColor = 'blue';
+      showHoliday = true;
+      
+      console.log(item);
+    });
+  });
+
+  function mudaCor(state) {
+	if (state == true) {
+		item.style.backgroundColor = 'blue';
+	} else {
+		item.style.backgroundColor = 'rgb(238,238,238)';
+	}
+}
+
+selectDaysHoliday();
+
+//4.
+
+createButton('Sexta-feira', 'btn-friday');
+
+//5.
+function selectDaysFriday() {
+  const buttonSelectFriday = document.getElementById('btn-friday');
+  buttonSelectFriday.addEventListener("click", function(){
+    document.getElementsByClassName("friday").innerText = "SEXTOU!"});;
+};
+
+selectDaysFriday();
+
+//6.
+
+
