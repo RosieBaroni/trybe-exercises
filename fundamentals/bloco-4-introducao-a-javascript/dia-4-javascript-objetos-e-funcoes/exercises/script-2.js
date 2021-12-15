14d6ba23d333b81fa3b6b70274a7e7dba7eb0e04
RosieBaroni <rosi.baroni@gmail.com>
2021-12-15 16:03:34 -0300

    exericios 4.4 finalizados

--- fundamentals/bloco-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercises/script-2.js
+++ fundamentals/bloco-4-introducao-a-javascript/dia-4-javascript-objetos-e-funcoes/exercises/script-2.js
@@ -1,6 +1,7 @@
 // Parte II - Funções
 
-//exercício 1.
+// 1. Crie uma função que receba uma string e retorne true se for um palíndromo, 
+//ou false , se não for.
 
 function verificaPalindrome(casa) {
   for (i in casa) {
@@ -8,7 +9,7 @@ function verificaPalindrome(casa) {
   }
 }   
 
-//exercício 2.
+// 2. Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
 function maiorPosicao(numeros) {
   let indiceMaior = 0;
@@ -24,23 +25,24 @@ function maiorPosicao(numeros) {
 
 maiorPosicao([4, 2, 20, 3]);
 
-//exercício 3.
+// 3.  Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 
-function maiorPosicao(numeros) {
-  let indiceMaior = 0;
+function menorPosicao(numeros) {
+  let indiceMenor = 0;
 
   for (let i in numeros) {
-    if (numeros[indiceMaior] > numeros[i]) {
-      indiceMaior = i;
+    if (numeros[indiceMenor] > numeros[i]) {
+      indiceMenor = i;
     }
   }
 
-  console.log(indiceMaior);
+  console.log(indiceMenor);
 }
 
-maiorPosicao([4, 2, 20, 3]);
+menorPosicao([4, 2, 20, 3]);
 
-//exercício 4.
+// 4. Crie uma função que receba um array de nomes e retorne o nome com a maior 
+//quantidade de caracteres.
 
 function maiorNome(nomes) {
   let maiorNome = "";
@@ -55,3 +57,63 @@ function maiorNome(nomes) {
 }
 
 maiorNome(['José', 'Fernanda', 'Joana']);
+
+// 5. Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+function numMaisRepete(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero += 1;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+  return numeros[indexNumeroRepetido];
+}
+
+console.log(numMaisRepete([2, 3, 2, 5, 8, 2, 3]));
+
+// 6. Crie uma função que receba um número inteiro N e retorne o somatório de todos 
+// os números de 1 até N.
+
+function somatorioAteN(n) {
+  let somaAteN = 0;
+
+  for (i = 1; i <= n; i += 1) {
+    somaAteN += i;
+  }
+
+  return somaAteN;
+}
+
+console.log(somatorioAteN(5));
+
+// 7. Crie uma função que receba uma string word e outra string ending. Verifique 
+// se a string ending é o final da string word . Considere que a string ending 
+// sempre será menor que a string word .
+
+function verificaFimPalavra(palavra, fimPalavra) {
+  palavra = palavra.split('');
+  fimPalavra = fimPalavra.split('');
+  controle = true;
+
+  for (let index = 0; index < fimPalavra.length; index += 1) {
+    if (palavra[palavra.length - fimPalavra.length + index] != fimPalavra[index]) {
+      controle = false;
+    }
+  }
+
+  return controle;
+}
+
+console.log(verificaFimPalavra('trybe', 'be'));
