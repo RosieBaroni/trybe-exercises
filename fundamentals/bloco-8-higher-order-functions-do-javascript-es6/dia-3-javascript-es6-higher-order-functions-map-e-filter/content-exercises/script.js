aad6707c63721532558c930ca4da8edf5f01c4ee
RosieBaroni <rosi.baroni@gmail.com>
2022-01-03 15:48:33 -0300

    exercises 8.3 completed

---
+++ fundamentals/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/content-exercises/script.js
@@ -0,0 +1,33 @@
+//USANDO A FUNÇÃO MAP
+
+// EXEMPLO 1.
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+// EXEMPLO 2.
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
+
+//A forEach não tem tal restrição. Ela pode modificar o array original e retorna 
+//nada por padrão - ela pode criar um array novo a partir de um antigo, pode 
+//simplesmente buscar por um elemento e retorná-lo, pode não retornar nada, enfim! 
+//Ela é genérica e pode fazer diversas coisas.
