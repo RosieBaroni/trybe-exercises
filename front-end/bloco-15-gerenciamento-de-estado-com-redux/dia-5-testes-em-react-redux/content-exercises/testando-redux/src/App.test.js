2b7de98aef9aa75b12ce019b3100fe77c25ff65f
RosieBaroni <rosi.baroni@gmail.com>
2022-03-03 16:14:57 -0300

    content-exercises-15.5

---
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-5-testes-em-react-redux/content-exercises/testando-redux/src/App.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux'
+import { render, cleanup } from '@testing-library/react';
+
+import clickReducer from './reducers';
+import App from './App';
+
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+  return {
+    ...render(<Provider store={store}>{component}</Provider>),
+    store,
+  }
+}
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  
+  test('the page should has a button and a text 0', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+  });
+});
